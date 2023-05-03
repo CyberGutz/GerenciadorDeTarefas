4a6384babbef302a00460b34185e92aea0fe024a
Gustavo <gutemagae@gmail.com>
2023-05-03 08:52:31 -0300

    Adição do drawer novamente, a primeira vez não funcionou corretamente

--- gerenciador_de_tarefas/lib/main.dart
+++ gerenciador_de_tarefas/lib/main.dart
@@ -36,68 +36,51 @@ class _MyHomePageState extends State<MyHomePage> {
     return Consumer<ThemeModel>(
         builder: (context, ThemeModel themeNotifier, child) {
       return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "Gerenciador De Tarefas",
-            textAlign: TextAlign.center,
-          ),
-          actions: [
-            IconButton(
-                icon: Icon(themeNotifier.isDark
-                    ? Icons.nightlight_round
-                    : Icons.wb_sunny),
-                onPressed: () {
-                  themeNotifier.isDark
-                      ? themeNotifier.isDark = false
-                      : themeNotifier.isDark = true;
-                })
-          ],
-        ),
-        body: ListView.builder(itemBuilder: (BuildContext context, int index) {
-          return Card(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            child: const Padding(
-              padding: EdgeInsets.all(12.0),
-              child: Text(
-                "Tarefa.",
-                style: TextStyle(fontSize: 14),
-              ),
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              "Gerenciador De Tarefas",
+              textAlign: TextAlign.center,
             ),
-          );
-        }),
-        drawer: Drawer(
-          child: ListView(
+            actions: [
+              IconButton(
+                  icon: Icon(themeNotifier.isDark
+                      ? Icons.nightlight_round
+                      : Icons.wb_sunny),
+                  onPressed: () {
+                    themeNotifier.isDark
+                        ? themeNotifier.isDark = false
+                        : themeNotifier.isDark = true;
+                  })
+            ],
+          ),
+          body:
+              ListView.builder(itemBuilder: (BuildContext context, int index) {
+            return Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  "Tarefa.",
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+            );
+          }),
+          drawer: Drawer(
+              child: ListView(
             padding: EdgeInsets.zero,
             children: [
               const DrawerHeader(
-                decoration: BoxDecoration(),
-                child: Text(
-                  'Gerenciador de Tarefas',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              ListTile(
-                title: const Text('Home (TODO List)'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Lembretes'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Alarmes'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Relógio'),
-                onTap: () {},
-              ),
+                  decoration: BoxDecoration(),
+                  child: Text('Gerenciador de Tarefas')),
+              ListTile(title: const Text('Home (TODO LIST)'), onTap: () {}),
+              ListTile(title: const Text('Lembretes'), onTap: () {}),
+              ListTile(title: const Text('Alarmes'), onTap: () {}),
+              ListTile(title: const Text('Relógio'), onTap: () {}),
             ],
-          ),
-        ),
-      );
+          )));
     });
   }
 }
