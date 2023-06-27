4182077de0463a42919c790fb4177f4bfe92491b
Gustavo <gutemagae@gmail.com>
2023-06-27 17:17:53 -0300

    Mudanças no tema

--- gerenciador_de_tarefas/lib/appTheme.dart
+++ gerenciador_de_tarefas/lib/appTheme.dart
@@ -13,13 +13,12 @@ class AppTheme {
       color: Colors.transparent,
       iconTheme: IconThemeData(
         size: 20.0,
-        color: Colors.white,
+        color: Colors.black,
       ),
     ),
-
     colorScheme: ColorScheme(
-      brightness: Brightness.dark,
-      background: Colors.black,
+      brightness: Brightness.light,
+      background: Colors.white,
       error: Colors.red,
       errorContainer: Colors.redAccent,
       inversePrimary: Colors.white,
@@ -48,185 +47,116 @@ class AppTheme {
       shadow: Colors.white30,
       surfaceVariant: Colors.grey.shade900,
     ),
-
     iconTheme: const IconThemeData(
       size: 20,
-      color: Colors.white,
+      color: Colors.black,
     ),
-
     textTheme: const TextTheme(
       displayLarge: TextStyle(
         color: Colors.black,
         fontFamily: "Roboto",
         fontSize: 57.0,
-        height: 64,
         wordSpacing: 0,
       ),
       displayMedium: TextStyle(
         color: Colors.black,
         fontFamily: "Roboto",
         fontSize: 45.0,
-        height: 52,
         wordSpacing: 0,
       ),
       displaySmall: TextStyle(
         color: Colors.black,
         fontFamily: "Roboto",
         fontSize: 36.0,
-        height: 44,
         wordSpacing: 0,
       ),
       headlineLarge: TextStyle(
         color: Colors.black,
         fontFamily: "Roboto",
         fontSize: 32.0,
-        height: 40,
         wordSpacing: 0,
       ),
       headlineMedium: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 28.0,
-        height: 36,
         wordSpacing: 0,
       ),
       headlineSmall: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 24.0,
-        height: 32,
         wordSpacing: 0,
       ),
       titleLarge: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 22.0,
-        height: 28,
         wordSpacing: 0,
       ),
       titleMedium: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 16.0,
-        height: 24,
         wordSpacing: 0.15,
       ),
       titleSmall: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 16.0,
-        height: 20,
         wordSpacing: 0.1,
       ),
       bodyLarge: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 16.0,
-        height: 24,
         wordSpacing: 0.15,
       ),
       bodyMedium: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 14.0,
-        height: 20,
         wordSpacing: 0.25,
       ),
       bodySmall: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 12.0,
-        height: 16,
         wordSpacing: 0.4,
       ),
       labelLarge: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 14.0,
-        height: 20,
         wordSpacing: 0.1,
       ),
       labelMedium: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 12.0,
-        height: 16,
         wordSpacing: 0.5,
       ),
       labelSmall: TextStyle(
-        color: Colors.black,
+        color: Colors.white,
         fontFamily: "Roboto",
         fontSize: 11.0,
-        height: 16,
         wordSpacing: 0.5,
       ),
     ),
-
-    // textButtonTheme: const TextButtonThemeData(
-
-    // ),
-
-    // textSelectionTheme: const TextSelectionThemeData(),
-
-    // buttonTheme: const ButtonThemeData(),
-
-    // cardTheme: const CardTheme(),
-
-    checkboxTheme: const CheckboxThemeData(),
-
     disabledColor: const Color.fromARGB(255, 93, 108, 115),
-
     dividerTheme: const DividerThemeData(
-      color: Colors.white,
+      color: Color.fromRGBO(141, 141, 141, 1),
     ),
-
     drawerTheme: const DrawerThemeData(
-      backgroundColor: Colors.black,
+      backgroundColor: Color.fromRGBO(17, 17, 17, 1),
     ),
-
-    // dropdownMenuTheme: const DropdownMenuThemeData(),
-
-    // elevatedButtonTheme: const ElevatedButtonThemeData(),
-
-    // expansionTileTheme: const ExpansionTileThemeData(),
-
-    // filledButtonTheme: const FilledButtonThemeData(),
-
-    // floatingActionButtonTheme: const FloatingActionButtonThemeData(),
-
+    cardTheme: CardTheme(
+      color: Colors.grey.shade800,
+    ),
     hintColor: Colors.grey.shade400,
-
-    // iconButtonTheme: const IconButtonThemeData(),
-
-    // inputDecorationTheme: const InputDecorationTheme(),
-
-    // listTileTheme: const ListTileThemeData(),
-
-    // menuBarTheme: const MenuBarThemeData(),
-
-    // menuTheme: const MenuThemeData(),
-
-    // navigationBarTheme: const NavigationBarThemeData(),
-
-    // navigationDrawerTheme: const NavigationDrawerThemeData(),
-
     platform: TargetPlatform.android,
-
-    // popupMenuTheme: const PopupMenuThemeData(),
-
-    // scrollbarTheme: const ScrollbarThemeData(),
-
-    // snackBarTheme: const SnackBarThemeData(),
-
-    // switchTheme: const SwitchThemeData(),
-
-    // tabBarTheme: const TabBarTheme(),
-
-    // timePickerTheme: const TimePickerThemeData(),
-
-    // tooltipTheme: const TooltipThemeData(),
-
     scaffoldBackgroundColor: Colors.black,
+    useMaterial3: true,
   );
 
   static final ThemeData darkTheme = ThemeData(
